76d9d018567691522f70e757351f0b7cde189a65
Ricci-ricci <riccireese13@gmail.com>
2026-03-27 12:56:07 +0300

    simple log analyzer

---
+++ log_analyser.py
@@ -0,0 +1,13 @@
+with open("log_example.txt", "r", encoding="utf-8") as file:
+    logins = file.readlines()
+
+fail_count = {}
+for log in logins:
+    status, ip = log.split()
+    if status == "FAIL":
+        fail_count[ip] = fail_count.get(ip, 0) + 1
+        print(fail_count)
+
+for ip, count in fail_count.items():
+    if count >= 2:
+        print(f"[!!] Suspicious activities from {ip}")
